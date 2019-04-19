e77e2055da29145a431b74258683c250b022429d
Ki <kikim1025@gmail.com>
2019-04-19 00:04:39 -0400

    basic get post method for User api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,4 +5,40 @@ const bcrypt = require('bcryptjs');
 
 module.exports = function(app) {
 
-}
+// API routes for User ----------
+
+    // Get Users' username data
+    app.get('/api/user', function(req, res) {
+        User.find({}, 'username')
+        .then(function (data) {
+            res.json({ status: 200, data: data, message: 'All usernames retrieved successfully' });
+        })
+        .catch(function (err) {
+            res.json({ status: 500, message: err });
+        });        
+    });
+
+    // Create a User
+    // need to add JWT login after
+    app.post('/api/user', function (req, res) {
+        User.create(req.body)
+        .then(function (data) {
+            res.json({ status: 200, data: data.username, message: 'User created successfully' });
+        })
+        .catch(function (err) {
+            if (err.name === 'MongoError') { //  errors resulting from schema specification are handled here
+                res.json({ status: 403, message: err });
+            } else {
+                res.json({ status: 500, message: err });
+            }
+        });
+    });
+
+    // Deleting function maybe.
+    // need to think about removing all merchandise by user as well on deleting User
+
+
+// API routes for Merchandise ----------
+    
+
+};
